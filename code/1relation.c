46bc4515c6a1c1eb2e4f91ce317ee0243c838caa
Rachit-veronica <rachitgupta7007@gmail.com>
2021-04-29 17:00:24 +0530

    relation

---
+++ code/1relation.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a number");
+	scanf("%d",&b);
+	a=34>b>0;
+	printf("a=%d",a);
+	return 0;
+}
---
+++ code/relation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int x,b;
+	printf("enter any number");
+	scanf("%d",&b);
+	x=5>b;
+	printf("a=%d",x);
+	return 0;
+}
+
