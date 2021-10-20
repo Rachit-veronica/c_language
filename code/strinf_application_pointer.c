671af168162c4bf89f4ce9e1705aa76eab5b35e4
rachit-veronica <rachitgupta7007@gmail.com>
2021-10-20 13:37:48 +0530

    string_in_pointer

---
+++ code/strinf_application_pointer.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+char* reverse(char *p)
+{
+	int l,i;
+	char t;
+	for(l=0;*(p+l)!='\0';l++);
+		for(i=0;i<l/2;i++)
+		{
+			t=*(p+i); // *(p+1)=s[0]
+			*(p+i)=*(p+l-1-i); // *(p+l-1-i) 
+			*(p+l-1-i)=t; 
+		}
+	return(p);
+}
+int length(char *p)
+{
+	int i;
+	for(i=0;*(p+i)!='\0';i++);
+	return (i);
+}
+int main()
+{
+printf("\n%d\n",length("computer"));
+printf("%s\n",reverse("computer"));
+return 0;
+}
